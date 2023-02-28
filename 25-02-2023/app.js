18f5c72473abcb93998cb343215d0e023a05baad
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2023-02-28 14:58:44 +0500

    25-02-2023

---
+++ 25-02-2023/app.js
@@ -0,0 +1,110 @@
+// let item=prompt("Kia laye ho?")
+
+// if(item==="tomato"){
+//     document.write("Shabash tum tomato le aye")
+// }
+// else if(item==="dahi"){
+//     document.write("Chlo dahi bh thek hai")
+// }
+// else if(item==="onion"){
+//     document.write("Chlo onion bh thek hai")
+// }
+// else if(item==="Dhaniya"){
+//     document.write("Chlo dhaniya bh thek hai")
+// }
+// else{
+//     document.write("Ghar mat ana")
+// }
+
+// let age=+prompt("Enter your age")
+// let country=prompt("Enter your country")
+// let city=prompt("Enter your city")
+
+// if(age>=18 && country==="pakistan"){
+//     alert("You are eligible to vote")
+// }
+// else if(age>=18 && country!=="pakistan"){
+//     alert("You should be a resident of pakistan")
+// }
+// else if(age<18 && country==="pakistan"){
+//     alert("age should be greater than or equal to 18")
+// }
+// else if(age<18 && country!=="pakistan"){
+//     alert("You cannot vote")
+// }
+// else{
+//     alert("Invalid input")
+// }
+// let email=prompt("Enter email")
+// let password=prompt("Enter password")
+
+// if(email==="someone@example.com"){
+//     if(password==="12345"){
+//         document.write("Login Successful")
+//     }
+//     document.write("Pasword is incorrect")
+// }
+
+
+
+// else if(email!=="someone@example.com"){
+// document.write("Your email is not corect")
+// }
+// else{
+//     document.write("Email or password is incorrect")
+// }
+
+// let letter=prompt("Enter a letter")
+
+// switch(letter){
+
+//     case "a":
+//         document.write("A is a vowel")
+//         break
+
+//     case "e":
+//         document.write("E is a vowel")
+//         break
+
+//     case "i":
+//         document.write("I is a vowel")
+//         break
+
+//     case "o":
+//         document.write("O is a vowel")
+//         break
+
+//     case "u":
+//         document.write("U is a vowel")
+//         break
+    
+//     default:
+//         document.write("Entered letter is a consonant")
+//         break
+
+
+// }
+
+// let choice=+prompt("Enter 1 for area of triangle. Enter 2 for area of rectangle")
+
+// switch(choice){
+//     case 1:
+//         let height=prompt("Enter height of triangle")
+//         let base=prompt("Enter base of triangle")
+//         let area_of_triangle=(1/2)*parseFloat(base)*parseFloat(height)
+//         document.write("Area of triangle is: "+area_of_triangle)
+//         break
+//     case 2:
+//         let length=prompt("Enter length")
+//         let breadth=prompt("Enter breadth")
+//         let area_of_rectangle=parseFloat(length)*parseFloat(breadth)
+//         document.write("Area of rectangle is: "+area_of_rectangle)
+//         break
+//     default:
+//         document.write("Invalid Input")
+//         break
+
+
+
+// }
+
